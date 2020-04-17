3bf011980bf30e697c62a037e0eb6861e4203ec9
sim manseop <tlqaksqhr@naver.com>
2020-04-17 23:47:59 +0900

    add login check(session check) logic using interceptor and setting logic

---
+++ src/main/java/com/semtax/application/config/WebserviceConfig.java
@@ -0,0 +1,21 @@
+package com.semtax.application.config;
+
+import com.semtax.application.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebserviceConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/post/**")
+                .addPathPatterns("/comment/**");
+    }
+}
---
+++ src/main/java/com/semtax/application/interceptor/LoginInterceptor.java
@@ -0,0 +1,43 @@
+package com.semtax.application.interceptor;
+
+
+import com.semtax.application.util.Sessions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
+
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession httpSession = request.getSession();
+        String sessionItem = (String)httpSession.getAttribute(Sessions.SESSION_ID);
+
+        if(sessionItem == null){
+            response.getOutputStream().println("LOGIN REQUIRED!");
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
